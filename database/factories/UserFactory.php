ddc31c369b60a68b24e253b1dfa0057d7359bfcf
poegim <jacekwiniarczyk@gmail.com>
2026-03-10 12:19:41 +0100

    UserFactory fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,9 +23,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create();
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -33,6 +34,7 @@ class UserFactory extends Factory
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
         ];
+    }
     }
 
     /**
