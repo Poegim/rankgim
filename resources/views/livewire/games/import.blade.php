00c78b49b986b94df36c549122baed1beecb8046
poegim <jacekwiniarczyk@gmail.com>
2026-03-22 10:47:10 +0100

    import games modal show fix

--- resources/views/livewire/games/import.blade.php
+++ resources/views/livewire/games/import.blade.php
@@ -76,7 +76,10 @@
                                              class="w-5 h-3.5 rounded-sm shrink-0">
                                         <span class="font-medium text-zinc-800 dark:text-white">{{ $row['winner']['name'] }}</span>
                                     @else
-                                        <span class="text-red-500 cursor-pointer" x-on:click.stop="navigator.clipboard.writeText('{{ $row['winner_name'] }}')">{{ $row['winner_name'] }} <span class="text-xs">(not found)</span></span>
+                                        <span class="text-red-500">{{ $row['winner_name'] }} <span class="text-xs">(not found)</span></span>
+                                        <button type="button" x-on:click.stop="navigator.clipboard.writeText('{{ $row['winner_name'] }}')" class="text-zinc-400 hover:text-zinc-200 ml-1" title="Copy name">
+                                            <svg class="w-3.5 h-3.5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/></svg>
+                                        </button>
                                     @endif
                                 </button>
                             @endif
@@ -91,7 +94,10 @@
                                              class="w-5 h-3.5 rounded-sm shrink-0">
                                         <span class="font-medium text-zinc-800 dark:text-white">{{ $row['loser']['name'] }}</span>
                                     @else
-                                        <span class="text-red-500 cursor-pointer" x-on:click.stop="navigator.clipboard.writeText('{{ $row['loser_name'] }}')">{{ $row['loser_name'] }} <span class="text-xs">(not found)</span></span>
+                                        <span class="text-red-500">{{ $row['loser_name'] }} <span class="text-xs">(not found)</span></span>
+                                        <button type="button" x-on:click.stop="navigator.clipboard.writeText('{{ $row['loser_name'] }}')" class="text-zinc-400 hover:text-zinc-200 ml-1" title="Copy name">
+                                            <svg class="w-3.5 h-3.5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/></svg>
+                                        </button>
                                     @endif
                                 </button>
                             @endif
